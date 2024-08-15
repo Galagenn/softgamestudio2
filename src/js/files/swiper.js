52df94272bb5b0ff8aa0f9d6067f9482da974c35
Mukhammed <massalimov.mukhammed@mail.ru>
2024-08-15 11:42:02 +0500

    Services section

--- src/js/files/swiper.js
+++ src/js/files/swiper.js
@@ -1,18 +1,58 @@
 import Swiper from "swiper";
-import { Navigation } from "swiper/modules";
+import { Autoplay } from "swiper/modules";
+
+import "swiper/scss";
 
 function initSliders() {
-   if (document.querySelector(".swiper")) {
-      new Swiper(".swiper", {
-         modules: [Navigation],
+   if (document.querySelector(".services__slider")) {
+      new Swiper(".services__slider", {
+         modules: [Autoplay],
          observer: true,
          observeParents: true,
-         slidesPerView: 1,
-         spaceBetween: 0,
+         slidesPerView: 2.5,
+         spaceBetween: 230,
          speed: 800,
-         navigation: {
-            prevEl: ".swiper-button-prev",
-            nextEl: ".swiper-button-next",
+         centeredSlides: true,
+         loop: true,
+         autoplay: {
+            delay: 2000,
+            disableOnInteraction: false,
+         },
+         breakpoints: {
+            320: {
+               slidesPerView: 1.1,
+               spaceBetween: 10,
+               centeredSlides: false,
+            },
+            480: {
+               slidesPerView: 1.5,
+               spaceBetween: 15,
+               centeredSlides: false,
+            },
+            650: {
+               slidesPerView: 1.8,
+               spaceBetween: 20,
+            },
+            768: {
+               slidesPerView: 2.1,
+               spaceBetween: 40,
+            },
+            992: {
+               slidesPerView: 2.2,
+               spaceBetween: 50,
+            },
+            1024: {
+               slidesPerView: 2.2,
+               spaceBetween: 50,
+            },
+            1324: {
+               slidesPerView: 2.4,
+               spaceBetween: 100,
+            },
+            1476: {
+               slidesPerView: 2.5,
+               spaceBetween: 230,
+            }
          },
       });
    }
